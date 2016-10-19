ce252ae4b8d142f88c47033b34d7153f86f94c6d
Frank Vinci <fv42wid@yahoo.com>
2016-10-19 08:40:12 +0000

    setting up fog and s3

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      #config for aws s3
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_ACCESS_KEY'],
+      :region => ENV['S3_REGION']
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+  end
+end
