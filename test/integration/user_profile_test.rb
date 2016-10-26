627ea764586ce3274df088c9b6bdf8829a3e7fa0
Frank Vinci <fv42wid@yahoo.com>
2016-10-26 18:07:31 +0000

    test profile picture upload, user show view next

--- test/integration/user_profile_test.rb
+++ test/integration/user_profile_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UserProfileTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   # test "the truth" do
   #   assert true
   # end
@@ -20,5 +21,20 @@ class UserProfileTest < ActionDispatch::IntegrationTest
     assert_nil @frank.profile
     assert_redirected_to new_user_session_path
     assert_equal "You need to sign in or sign up before continuing.", flash[:alert]
+  end
+
+  #verified user can upload picture
+  test "verified user can upload profile picture" do
+    assert_nil @frank.profile
+    file = Rack::Test::UploadedFile.new(@profile_image, "image/jpg")
+    sign_in @frank
+    get edit_user_path(@frank)
+    assert_response :success
+    patch profile_path, params: {profile: {picture: file} }
+    @frank.reload
+    assert @frank.profile.picture.path
+    assert_redirected_to user_path(@frank)
+    follow_redirect!
+    assert_select "img[src=?]", @frank.profile.picture.url
   end
 end
